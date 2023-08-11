8a3a3a0c2055fcd69b6dfa8e5c8974a0153e9347
Raghu-KV <raghunandanv19@gmail.com>
2023-08-11 16:36:59 +0530

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+function HeroSection() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="bg-slate-800 h-4/6 w-5/6 mt-20 rounded-3xl drop-shadow-xl"></div>
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,91 @@
+import { useState, useRef, useEffect } from "react";
+
+function NavBar() {
+  const [nav, setNav] = useState(false);
+  const navRef = useRef();
+
+  useEffect(() => {
+    const handler = (event) => {
+      if (!navRef.current.contains(event.target)) {
+        setNav(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
+
+  return (
+    <nav className="bg-gray-300 fixed w-full">
+      <div className="container px-5 mx-auto flex items-center justify-between md:justify-start font-semibold h-20">
+        <div className="cursor-pointer mr-24">LOGO</div>
+        <div className="hidden md:flex md:items-center w-full">
+          <ul className="flex gap-4 cursor-pointer">
+            <li className="hover:text-sky-800 duration-300">Home</li>
+            <li className="hover:text-sky-800 duration-300">About Us</li>
+            <li className="hover:text-sky-800 duration-300">Services</li>
+          </ul>
+          <div className="cursor-pointer hover:bg-sky-900 duration-300 bg-sky-800 py-2 px-5 rounded-lg text-white ml-auto">
+            <i className="fa-solid fa-phone mr-2"></i>
+            <span className="">Contact</span>
+          </div>
+        </div>
+        <div ref={navRef}>
+          <div className="md:hidden">
+            <div className="relative z-10" onClick={() => setNav(!nav)}>
+              {nav ? (
+                <i className="fa-solid fa-xmark fa-xl"></i>
+              ) : (
+                <i className="fa-solid fa-bars fa-xl"></i>
+              )}
+            </div>
+
+            <div
+              className={`bg-gray-300 absolute w-2/3 h-screen top-0 right-0 z-0 duration-150 flex justify-center items-center drop-shadow-xl ${
+                nav ? "translate-x-0" : "translate-x-full"
+              } `}
+            >
+              <ul className="flex flex-col items-center gap-4 cursor-pointer">
+                <li
+                  className="hover:text-sky-800 duration-300"
+                  onClick={() => setNav(false)}
+                >
+                  Home
+                </li>
+                <li
+                  className="hover:text-sky-800 duration-300"
+                  onClick={() => setNav(false)}
+                >
+                  About Us
+                </li>
+                <li
+                  className="hover:text-sky-800 duration-300"
+                  onClick={() => setNav(false)}
+                >
+                  Services
+                </li>
+                <li
+                  className="hover:text-sky-800 duration-300"
+                  onClick={() => setNav(false)}
+                >
+                  Portfolio
+                </li>
+                <li
+                  className=" duration-300 hover:bg-sky-900 bg-sky-800 py-2 px-5 rounded-lg text-white"
+                  onClick={() => setNav(false)}
+                >
+                  <i className="fa-solid fa-phone mr-2"></i>Contact
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
