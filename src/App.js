e7d89bc81e806ee1737c765d113d44549c872e8c
Raghu-KV <raghunandanv19@gmail.com>
2023-08-16 20:06:00 +0530

    tes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import AboutUs from "./components/AboutUs";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <HeroSection />
       <AboutUs />
+      <Services />
     </div>
   );
 }
