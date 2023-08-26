d4be8e5734182e7658fdb7180d143dfe346ddc8d
Raghu-KV <raghunandanv19@gmail.com>
2023-08-26 07:56:04 +0530

    bg

--- src/components/SingleServices.jsx
+++ src/components/SingleServices.jsx
@@ -1,6 +1,6 @@
 function SingleServices({ item }) {
   return (
-    <div className="border-[3px] py-5 rounded-lg flex flex-col items-center justify-center gap-3 min-h-[200px] hover:border-[3px] border-sky-800 hover:!blur-none hover:!scale-100 duration-200 cursor-pointer group-hover:blur-[2px] group-hover:scale-[0.95]">
+    <div className="border-[3px] py-5 rounded-2xl flex flex-col items-center justify-center gap-3 min-h-[200px] hover:border-[3px] border-sky-800 hover:!blur-none hover:!scale-100 duration-200 cursor-pointer group-hover:blur-[2px] group-hover:scale-[0.95]">
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
